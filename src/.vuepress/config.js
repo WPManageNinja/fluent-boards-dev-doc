c1d1db4a553e8ab18d6ed89dbaf4ba5bc9dd5b67
Masiur <mrsiddiki@gmail.com>
2025-01-15 11:27:54 +0600

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -70,7 +70,7 @@ module.exports = {
         searchMaxSuggestions: 10,
         algolia: {
             apiKey: 'f07487500b8e8e1456d054f87a2ce71f',
-            indexName: 'fluent_board_dev_docs_crawl_pages',
+            indexName: 'developers-fluentboards',
             appId: 'BFUQ6C81LB',
         },
         nav: [
